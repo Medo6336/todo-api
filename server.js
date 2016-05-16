2c74ef3cca775611780c1bb39ab77f8298a93d7b
Medo M <medo6336@gmail.com>
2016-05-15 20:25:34 -0400

    added 3rd todo item

--- server.js
+++ server.js
@@ -6,8 +6,31 @@ var app = express();
 var port = process.env.PORT || 6336;
 
 
+var todos = [
+    {
+        id: 1,
+        description: 'eat',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'drink water',
+        completed: true
+    },
+    {
+        id: 3,
+        description: 'wiggle',
+        completed: false
+    }
+];
+
 app.get('/', function (request, response) {
     response.send('todo-api root page');
+});
+
+
+app.get('/todos', function (request, response) {
+    response.json(todos);
 });
 
 app.listen(port, function () {
